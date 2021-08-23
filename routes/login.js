d7b8e62f92e9abd494dc6f71c2f65ae1c5f9eac7
Ahmed <achau1019@gmail.com>
2021-08-23 18:00:32 +0000

    change endpoint logic

--- routes/login.js
+++ routes/login.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = () => {
-  router.get("/login", (req, res) => {
+  router.get("/", (req, res) => {
     res.render('login');
   });
   return router;
--- routes/register.js
+++ routes/register.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = () => {
-  router.get("/register", (req, res) => {
+  router.get("/", (req, res) => {
     res.render('register');
   });
   return router;
