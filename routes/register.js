dd2c832114cb51df7e038d07431a432a550d11a6
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-23 15:36:21 +0000

    adjusted register get route

--- routes/register.js
+++ routes/register.js
@@ -3,7 +3,7 @@ const express = require('express');
 const router  = express.Router();
 
 module.exports = () => {
-  router.get("/", (req, res) => {
+  router.get("/register", (req, res) => {
     res.render('register');
   });
   return router;
