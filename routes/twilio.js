7a5bc7de93fa6b99cd1ea005e9e198fa54c8f5a0
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-26 03:18:21 +0000

    creating functions that are able to make outgoing sms messages

--- routes/twilio.js
+++ routes/twilio.js
@@ -4,16 +4,40 @@ require('dotenv').config();
 
 const client = require('twilio')();
 
-const sendMessageToRestaurant = (order) => {
+const messageRestaurant = (order) => {
   client.messages.create({
     to: process.env.MY_PHONE_NUMBER,
     from: '+14377476201',
-    body: `You have received an order of ${order}`
+    body: `Hi! You have received an order of ${order}! Respond to this message with the amount of time it'll take to complete the order. (ex. entering 30 would mean 30 minutes)`
   })
   .then((message) => console.log(message.sid));
 }
 
-sendMessageToRestaurant('coffee');
+const messageCustomerUpdate = (customerName, time) => {
+  client.messages.create({
+    to: process.env.MY_PHONE_NUMBER,
+    from: '+14377476201',
+    body: `Hi ${customerName}! Your order will be ready in ${time} mins :)`
+  })
+  .then((message) => console.log(message.sid));
+
+}
+
+const messageCustomerComplete= (customerName) => {
+  client.messages.create({
+    to: process.env.MY_PHONE_NUMBER,
+    from: '+14377476201',
+    body: `Hi ${customerName}! Your order is now ready for pickup. Enjoy! :)`
+  })
+  .then((message) => console.log(message.sid));
+}
+
+messageCustomerComplete("bob");
 
 
 
+
+
+
+
+module.exports = {messageCustomerComplete, messageCustomerUpdate, messageRestaurant};
