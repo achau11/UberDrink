4ae534236584ba11c6e6aa1a4c121431894efbdd
Ahmed <achau1019@gmail.com>
2021-08-26 15:54:54 +0000

    Display items in database, change url paths database

--- db/seeds/03_items.sql
+++ db/seeds/03_items.sql
@@ -1,5 +1,5 @@
-INSERT INTO items (name, imgurl, cost) VALUES ('Hamburger', '../../public/burger.jpeg', 7.50);
-INSERT INTO items (name, imgurl, cost) VALUES ('Birthday Cake', '../../public/cake.jpeg', 15.00);
-INSERT INTO items (name, imgurl, cost) VALUES ('Black Coffee', '../../public/coffee.jpeg', 3.25);
-INSERT INTO items (name, imgurl, cost) VALUES ('Coke Bottle', '../../public/coke.jpeg', 2.00);
-INSERT INTO items (name, imgurl, cost) VALUES ('Salad', '../../public/salad.jpeg', 2.00);
+INSERT INTO items (name, imgurl, cost) VALUES ('Hamburger', 'images/burger.jpeg', 7.50);
+INSERT INTO items (name, imgurl, cost) VALUES ('Birthday Cake', 'images/cake.jpeg', 15.00);
+INSERT INTO items (name, imgurl, cost) VALUES ('Black Coffee', 'images/coffee.jpeg', 3.25);
+INSERT INTO items (name, imgurl, cost) VALUES ('Coke Bottle', 'images/coke.jpeg', 2.00);
+INSERT INTO items (name, imgurl, cost) VALUES ('Salad', 'images/salad.jpeg', 2.00);
--- routes/order.js
+++ routes/order.js
@@ -3,16 +3,11 @@ const router  = express.Router();
 
 module.exports = (db) => {
   router.get("/", (req, res) => {
-    if (req.session.user_id) {
-      db.query('SELECT * FROM users WHERE id = $1', [req.session.user_id])
+      db.query('SELECT * FROM items')
       .then((result) => {
-        const templateVars = {user: result.rows[0]};
-        res.render("order", templateVars);
+        res.render("order", {items: result.rows});
       })
       .catch(err => console.log(err.message));
-    } else {
-      res.render("order", {user: null});
-    }
-  });
+    });
   return router;
 };
