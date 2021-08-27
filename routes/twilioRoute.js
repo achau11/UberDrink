57cd0da75ef2176d4f9819f6aae8c6a71e14c95f
Yusuf Abdulla <y.abdulla37@gmail.com>
2021-08-27 01:28:03 +0000

    added order status time to the status page

--- routes/twilioRoute.js
+++ routes/twilioRoute.js
@@ -6,18 +6,27 @@ const MessagingResponse = require('twilio').twiml.MessagingResponse;
 const {messageCustomerComplete, messageCustomer, messageRestaurant} = require('../public/scripts/twilio')
 
 module.exports = () => {
+  let waitTime = 0;
   router.post('/', (req, res) => {
     const twiml = new MessagingResponse();
     twiml.message(messageRestaurant());
-
+console.log('route hit');
     res.render('status', {time: null});
   });
 
   router.post('/received', (req, res) => {
     const twiml = new MessagingResponse();
     twiml.message(messageCustomer('+13432971071', req.body.Body));
+    console.log('received', req.body.Body);
+    waitTime = req.body.Body;
+    res.redirect('/');
 
-    res.render('status', {time: req.body.Body});
+  });
+
+  router.get('/', (rq, res)=> {
+    console.log('get route')
+    console.log('waitTime: ', waitTime);
+    res.render('status', {time: waitTime});
   });
 
   return router;
